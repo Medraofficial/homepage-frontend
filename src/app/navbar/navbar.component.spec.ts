621a61ca7b2ded41e8a33b8f66cbb35942aa75d3
Jannick Born <Jannick-born@t-online.de>
2022-09-03 22:45:05 +0200

    Router testing

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -14,6 +15,11 @@ describe('NavbarComponent', () => {
   });
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [],
+      declarations: [],
+    })
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
