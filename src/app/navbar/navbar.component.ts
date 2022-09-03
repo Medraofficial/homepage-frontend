0284da014efd2ab71903b055ac9beaac9ca7e5f5
Jannick Born <Jannick-born@t-online.de>
2022-09-03 22:22:53 +0200

    Mouseover & highlighting of categories

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    this.checkRoute()
+  }
+
+  checkRoute() {
+    switch(this.router.url)
+    {
+      case "/news": 
+      {
+        const news = document.getElementById('news');
+        if (news != null)
+          news.style.color = getComputedStyle(document.documentElement).getPropertyValue('--hover-color');   
+        break;
+      }
+
+      case "/music":
+      {
+        const music = document.getElementById('music');
+        if (music != null)
+          music.style.color = getComputedStyle(document.documentElement).getPropertyValue('--hover-color');   
+        break;
+      }
+
+      case "/merch":
+      {
+        const merch = document.getElementById('merch');
+        if (merch != null)
+          merch.style.color = getComputedStyle(document.documentElement).getPropertyValue('--hover-color');   
+        break;
+      }
+
+      case "/band":
+      {
+        const band = document.getElementById('band');
+        if (band != null)
+          band.style.color = getComputedStyle(document.documentElement).getPropertyValue('--hover-color');   
+        break;
+      }
+    }
   }
 
 }
