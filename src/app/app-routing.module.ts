f86d21ffd69911c59474f2b0d3e3bd07447fc437
Jannick Born <Jannick-born@t-online.de>
2022-09-03 21:25:42 +0200

    Routing & links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BandComponent } from './band/band.component';
+import { MerchComponent } from './merch/merch.component';
+import { MusicComponent } from './music/music.component';
+import { NewsComponent } from './news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: NewsComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'music', component: MusicComponent},
+  {path: 'merch', component: MerchComponent},
+  {path: 'band', component: BandComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BandComponent } from './band/band.component';
+import { NewsComponent } from './news/news.component';
+import { MusicComponent } from './music/music.component';
+import { MerchComponent } from './merch/merch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    BandComponent,
+    NewsComponent,
+    MusicComponent,
+    MerchComponent
   ],
   imports: [
     BrowserModule,
